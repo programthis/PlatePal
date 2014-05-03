ea2e8b8081db4811ec72d68e3fe9f5ac2cd88e53
programthis <kylebachan@gmail.com>
2014-05-03 13:32:45 -0400

    Added welcome landing page and updated it with user profile info

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 PlatePal::Application.routes.draw do
   
-  
   resources :patients
   resources :sessions
+  resources :welcome
 
-  root to: "sessions#new"
+  root to: "welcome#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
