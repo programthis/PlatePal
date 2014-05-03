e9c512c7789de13066285c97613e0e5e8506ae1a
programthis <kylebachan@gmail.com>
2014-05-03 10:27:33 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PlatePal::Application.config.secret_key_base = 'c5f4aed9cb4e1ef513f34449096786e13c490810b6de80e3f8c34525ce2705b4e3f3086cc3a982e359386d20278586cff384b258a3e4005874ef43ad1b3d34b0'
