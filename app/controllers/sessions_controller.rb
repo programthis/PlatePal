c40a76aede5f5ef38e49b3ccd61aa352f43aaf9e
programthis <kylebachan@gmail.com>
2014-05-03 13:09:18 -0400

    Added sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,20 @@
 class SessionsController < ApplicationController
   def create
+  	patient = Patient.find_by_email(params[:email])
+  	if patient && patient.authenticate(params[:password])
+  		session[:patient_id] = patient.id
+  		redirect_to patient_path(patient), notice: "Logged in!"
+  	else
+  		flash.now[:alert] = "Invalid email or password"
+  		render :new
+  	end
   end
 
   def new
+  end
+
+  def destroy
+  	session.destroy
+  	redirect_to new_session_path, notice: "Congrats, you've logged out!"
   end
 end
