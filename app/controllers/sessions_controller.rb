7c2efec2d18684bcaa2827ce84029b021d915b15
programthis <kylebachan@gmail.com>
2014-06-07 14:06:57 -0400

    Updated adding new session for coach

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,13 @@
 class SessionsController < ApplicationController
   def create
   	patient = Patient.find_by_email(params[:email])
+    coach = Coach.find_by_email(params[:email])
   	if patient && patient.authenticate(params[:password])
   		session[:patient_id] = patient.id
   		redirect_to welcome_path(patient), notice: "Logged in!"
+    elsif coach && coach.authenticate(params[:password])
+      session[:coach_id] = coach.id
+      redirect_to welcome_path(coach), notice: "Logged in!"
   	else
   		flash.now[:alert] = "Invalid email or password"
   		render :new
